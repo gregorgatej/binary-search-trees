0f3458aa25b636fc745fd7f34c9c3cbdca06f450
Gregor Gatej <gregor.gatej@gmail.com>
2026-01-27 13:20:41 +0100

    feat: add pretty_print and insert methods

--- bst.rb
+++ bst.rb
@@ -21,8 +21,18 @@ class Tree
     @root = build_tree(array)
   end
 
+  def insert(value)
+    @root = insert_rec(@root, value)
+  end
+
+  def pretty_print(node = @root, prefix = '', is_left = true)
+    pretty_print(node.right, "#{prefix}#{is_left ? '│   ' : '    '}", false) if node.right
+    puts "#{prefix}#{is_left ? '└── ' : '┌── '}#{node.data}"
+    pretty_print(node.left, "#{prefix}#{is_left ? '    ' : '│   '}", true) if node.left
+  end
+
   private
-  
+
   def build_tree(array, start = 0, stop = array.length - 1)
     return nil if start > stop
 
@@ -33,5 +43,17 @@ class Tree
     root.right = build_tree(array, mid + 1, stop)
 
     root
+  end
+
+  def insert_rec(node, value)
+    return Node.new(value) if node.nil?
+
+    if value < node.data
+      node.left = insert_rec(node.left, value)
+    elsif value > node.data
+      node.right = insert_rec(node.right, value)
+    end
+
+    node
   end
 end
