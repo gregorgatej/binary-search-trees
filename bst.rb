df8928fbeec603a71aa84277d82c0bb51f49cdd6
Gregor Gatej <gregor.gatej@gmail.com>
2026-01-27 06:24:58 +0100

    feat: implement build_tree method

--- bst.rb
+++ bst.rb
@@ -17,10 +17,19 @@ class Tree
   attr_accessor :root
 
   def initialize(array)
+    array = array.uniq.sort
     @root = build_tree(array)
   end
 
-  def build_tree(array)
-    
+  def build_tree(array, start = 0, stop = array.length - 1)
+    return nil if start > stop
+
+    mid = (start + stop) / 2
+
+    root = Node.new(array[mid])
+    root.left = build_tree(array, start, mid - 1)
+    root.right = build_tree(array, mid + 1, stop)
+
+    root
   end
 end
